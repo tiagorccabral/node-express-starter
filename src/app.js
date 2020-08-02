0630535571a1913bfd241c512d04677c184c3a87
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-08-02 00:11:33 -0300

    :lock: Adds cors and helmet lib

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const routes = require('./routes/v1');
 
 const app = express();
 
+// adds helmet HTTP protection
+app.use(helmet());
+
+// Sets CORS and Accepted Origins
+app.use(cors());
+app.options('*', cors());
+
+// Sets backend routes
 app.use('/v1', routes);
 
 module.exports = app;

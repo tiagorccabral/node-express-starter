e0657e043262dbce267924a367b54f154ecae7a1
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-08-01 23:13:01 -0300

    :art: Install and configure Eslint and Prettier
    Configures eslint, prettier using airbnb configs. Also adds couple scripts to auto-lint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const app = require('./app');
 const config = require('./config/config.js');
 
-let server;
-
-server = app.listen(config.port, () => {
-    console.log(`Listening to port ${config.port}`)
-})
+app.listen(config.port, () => {
+  console.log(`Listening to port ${config.port}`);
+});

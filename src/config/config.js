bac590bd24466007e28a38e5c308832d63b6ccba
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-08-07 18:00:25 -0300

    :sparkles: Using dotenv and other environment configs

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,23 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const Joi = require('@hapi/joi');
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    PORT: Joi.number().default(3000),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
 module.exports = {
-  env: 'development',
-  port: 3000,
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
 };

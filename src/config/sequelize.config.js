0ee56101d2d39c8f89cb078958386fa45d3b4ee8
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-08-10 15:25:12 -0300

    :card_file_box: Adds sequelize.config file to load sequelize credentials

---
+++ src/config/sequelize.config.js
@@ -0,0 +1,19 @@
+const config = require('./config');
+
+// load the correct env configurations for DB
+const dbConfig = config.database;
+
+module.exports = {
+  development: {
+    database: dbConfig.development.db_name,
+    username: dbConfig.development.db_username,
+    password: dbConfig.development.db_password,
+    dialect: dbConfig.development.db_dialect,
+  },
+  test: {
+    database: dbConfig.test.db_name,
+    username: dbConfig.test.db_username,
+    password: dbConfig.test.db_password,
+    dialect: dbConfig.test.db_dialect,
+  },
+};

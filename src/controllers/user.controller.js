2fe9021ee14700bbdfe3378e5a1afe6afb135f44
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-08-17 14:03:59 -0300

    :sparkles: Done getAllUsers and createUsers controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,20 @@
+const httpStatus = require('http-status');
+
 const catchAsync = require('../utils/catchAsync');
 const { userService } = require('../services');
 
-const index = catchAsync(async (req, res) => {
+const getAllUsers = catchAsync(async (req, res) => {
   const data = await userService.getAllUsers();
   res.send(data);
 });
 
+const createUser = catchAsync(async (req, res) => {
+  const reqData = req.body;
+  const user = await userService.createUser(reqData);
+  res.status(httpStatus.CREATED).send(user);
+});
+
 module.exports = {
-  index,
+  getAllUsers,
+  createUser,
 };

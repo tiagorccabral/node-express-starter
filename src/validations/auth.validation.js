d1170b86f0f4f3758e6b5e34dbfa05c668a8a933
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-08-25 00:28:27 -0300

    :sparkles: Adds validation function to login body request

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -9,6 +9,14 @@ const register = {
   }),
 };
 
+const login = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+};
+
 module.exports = {
   register,
+  login,
 };

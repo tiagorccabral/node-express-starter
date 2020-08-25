701c09351f0cdc76d70ccb0624842423b2607f86
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-08-25 12:54:42 -0300

    :sparkles: Creates JWT generation service and links to code
    Added acces JWT generation and linked with verification and auth contorller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const httpStatus = require('http-status');
 
 const catchAsync = require('../utils/catchAsync');
-const { userService, authService } = require('../services');
+const { userService, authService, tokenService } = require('../services');
 
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
@@ -10,7 +10,8 @@ const register = catchAsync(async (req, res) => {
 
 const login = catchAsync(async (req, res) => {
   const user = await authService.loginUserWithEmailAndPassword(req.body);
-  res.send({ user });
+  const token = await tokenService.generateAuthTokens(user);
+  res.send({ user, token });
 });
 
 module.exports = {

50b356d59e00a8913d75d2585974c71fd64729b7
Nicholas Marques <nnmarques97@gmail.com>
2020-09-02 15:07:12 -0300

    🐛 fixed version get on root page

--- src/routes/v1/main.route.js
+++ src/routes/v1/main.route.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const mainController = require('../../controllers/main.controller');
-const authorize = require('../../middlewares/role');
 
 // root
-router.get('/', authorize(), mainController.version);
+router.get('/', mainController.version);
 
 module.exports = router;
